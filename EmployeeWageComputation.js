0f7cab424b8eaed0c0dd630f1a72bab4f6446d25
tomthomas1 <tomthomas8688@gmail.com>
2022-04-09 00:00:56 +0530

    Calculate wage for a month

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,13 +1,16 @@
 console.log("******** Welcome To Employee Wage Computation Application *********");
 /**
  * We are using the random fun to check if the employee has done part time or full time.
- * We are calculating the wage as hours * wage per hour
+ * We have created a function to calculate the working hours
+ * Then we are using a for loop to calculate total hours for a month 
+ * And then we multiple the total hours with wage per hour.
  */
  const IS_PART_TIME = 1;
  const IS_FULL_TIME = 2;
  const PART_TIME_HOURS = 4;
  const FULL_TIME_HOURS = 8;
  const WAGE_PER_HOUR = 20;
+ const NUM_OF_WORKING_DAYS = 20;
  
  let employeeHours = 0;
  let employeeCheck = Math.floor(Math.random() * 10) % 3;
@@ -29,6 +32,10 @@ console.log("******** Welcome To Employee Wage Computation Application *********
              return 0;
      }
  }
- employeeHours = getWorkingHours(employeeCheck);
- let employeeWage = employeeHours * WAGE_PER_HOUR;
- console.log("Employee Wage = " + employeeWage);
+ for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+    employeeCheck = Math.floor(Math.random() * 10) % 3;
+    employeeHours += getWorkingHours(employeeCheck);
+}
+
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);

631c6f4a6e8396870ef444866c5494ac3f8810eb
tomthomas1 <tomthomas8688@gmail.com>
2022-04-09 00:08:14 +0530

    Calculate wage till working hour is 160 or days is 20

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -2,7 +2,7 @@ console.log("******** Welcome To Employee Wage Computation Application *********
 /**
  * We are using the random fun to check if the employee has done part time or full time.
  * We have created a function to calculate the working hours
- * Then we are using a for loop to calculate total hours for a month 
+ * Then we are using a while loop to calculate total hours for a month or max hours
  * And then we multiple the total hours with wage per hour.
  */
  const IS_PART_TIME = 1;
@@ -11,8 +11,11 @@ console.log("******** Welcome To Employee Wage Computation Application *********
  const FULL_TIME_HOURS = 8;
  const WAGE_PER_HOUR = 20;
  const NUM_OF_WORKING_DAYS = 20;
+ const MAX_HOURS_IN_MONTH = 160;
  
  let employeeHours = 0;
+let totalEmployeeHours = 0;
+let totalWorkingDays = 0;
  let employeeCheck = Math.floor(Math.random() * 10) % 3;
  
  function getWorkingHours(employeeCheck){
@@ -32,10 +35,11 @@ console.log("******** Welcome To Employee Wage Computation Application *********
              return 0;
      }
  }
- for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+ while(totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     employeeCheck = Math.floor(Math.random() * 10) % 3;
-    employeeHours += getWorkingHours(employeeCheck);
+    totalEmployeeHours += getWorkingHours(employeeCheck);
 }
 
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmployeeHours + "\nEmployee Wage = " + employeeWage);
